fe4d47bbd2e5f46c446f85c1c9bad09371ad6612
Rachiken <z390124578@gmail.com>
2021-06-21 23:28:52 +0800

    plot function

--- algo.py
+++ algo.py
@@ -50,7 +50,7 @@ class PSO_Particle(Particle):
 
 class Algo:
     def __init__(self, dim: int=2, testing: str='Ackley'):
-        self.run = 1
+        self.run = 30
         self.dim = dim
         self.testing = testing
         self.lower_bound = DOMAIN[self.testing][0]
@@ -89,7 +89,7 @@ class PSO(Algo):
                 for d in range(self.dim):
                     lo1 = random.random()
                     lo2 = random.random()
-                    p.personal_v[d] = 0.7 * p.personal_v[d] + lo1 * 1.496180 * (p.personal_c[d] - p.coordination[d]) + lo2 * 1.496180 * (best_p.personal_c[d] - p.coordination[d])
+                    p.personal_v[d] = 0.729844 * p.personal_v[d] + lo1 * 1.496180 * (p.personal_c[d] - p.coordination[d]) + lo2 * 1.496180 * (best_p.personal_c[d] - p.coordination[d])
                     if p.personal_v[d] > 5.0:
                         p.personal_v[d] = 5.0
                     if p.personal_v[d] < -5.0:
@@ -111,24 +111,46 @@ class PSO(Algo):
                 if p.current < best_p.best:
                     best_p = p
 
+                if best_p.best < best_so_far:
+                    best_so_far = best_p.best
+
+                if current_evl < self.evaluation:
+                    current_best[current_evl] = best_so_far
+
                 current_evl += 1
-            print(best_p.best)
+            print(best_so_far)
+        return current_best
 
+    def run_algo(self):
+        recorder = np.zeros((self.run, self.evaluation), dtype=float)
+        # print(recorder)
+        # print(recorder.shape)
+        for i in range(self.run):
+            algo = PSO(dim=self.dim, testing=self.testing)
+            recorder[i] = algo.algo()
+            print(self.testing, 'run :', i)
 
-    def my_run(self):
-        self.algo()
+        # print(recorder)
+        # x = np.sum(recorder, axis=0)
+        x = np.mean(recorder, axis=0)
+        # print(x)
+
+        # x1 = np.arange(self.iteration)
+        # plt.plot(x1, x)
+        # plt.show()
+        pd.DataFrame(x).to_csv("./output/" + self.testing, header=None)
 
 # 1. Initialize the centra point, max movement distance, discount factor, reward factor
 # 2. Set some points around the central point [-1,1]
 # 3. p_e = p_e + the distance between center and edge point * (M_MAX * discount factor * reward factor)
 # 4. check current best and best so far, converge to the best point(center) by n steps.
 # 5. repeat step 2.
-class MyAlgo(Algo):
+class NMyAlgo(Algo):
 
     def __init__(self, dim: int=2, discount_factor: float=0.9965, reward_factor: float=1.009,
             testing: str='Ackley'):
         super(MyAlgo, self).__init__(dim, testing)
-        self.run = 1
+        self.run = 30
         self.population = []
         self.discount_factor = discount_factor
         self.reward_factor = reward_factor
@@ -190,6 +212,8 @@ class MyAlgo(Algo):
                 x[i] = self.population[i].coordination[0]
                 y[i] = self.population[i].coordination[1]
                 if current_evl < self.evaluation:
+                    if center.best < best_so_far:
+                        best_so_far = center.best
                     current_best[current_evl] = best_so_far
                 current_evl += 1
             if center.best < best_so_far:
@@ -240,11 +264,115 @@ class MyAlgo(Algo):
         # plt.show()
         pd.DataFrame(x).to_csv("./output/" + self.testing, header=None)
 
+class MyAlgo(Algo):
 
+    def __init__(self, dim: int=2, discount_factor: float=0.9965, reward_factor: float=1.009,
+            testing: str='Ackley'):
+        super(MyAlgo, self).__init__(dim, testing)
+        self.run = 30
+        self.population = []
+        self.discount_factor = discount_factor
+        self.reward_factor = reward_factor
+        self.max_mov = abs(self.upper_bound - self.lower_bound) * 0.5
+
+    def push(self, center) -> None:
+        for i in range(len(self.population)):
+            for d in range(self.dim):
+                self.population[i].coordination[d] = self.population[i].coordination[d] * self.max_mov + center.coordination[d]
+                if self.population[i].coordination[d] > self.upper_bound:
+                    self.population[i].coordination[d] += (center.coordination[d] - self.population[i].coordination[d])*2
+                elif self.population[i].coordination[d] < self.lower_bound:
+                    self.population[i].coordination[d] += (center.coordination[d] - self.population[i].coordination[d])*2
+
+    def pull(self, center) -> None:
+        for i in range(len(self.population)):
+            for d in range(self.dim):
+                pull_distance = random.uniform(center.coordination[d], self.population[i].coordination[d])
+                # pull from the point to the center point between half and center point range[p+c/2, c]
+                self.population[i].coordination[d] = pull_distance
+
+    def pull_2(self, center) -> None:
+        for i in range(len(self.population)):
+            for d in range(self.dim):
+                v = center.coordination[d] - self.population[i].coordination[d]
+                # pull from the point to the center point between half and center point range[p+c/2, c]
+                self.population[i].coordination[d] += v / 3
+
+    def algo(self):
+        best_so_far = 16777216
+        current_best = np.zeros(self.evaluation, dtype=float)
+        center = Particle(self.lower_bound, self.upper_bound, dim=self.dim)
+        # center2 = Particle(self.lower_bound, self.upper_bound, dim=self.dim)
+        center.best = globals()[self.testing.lower()](self.dim, center.coordination)
+        # center2.best = ackley(self.dim, center.coordination)
+        current_evl = 0
+        k=0
+        while current_evl < self.evaluation:
+            # 2. Set some points around the center point [-1,1]
+            if k % 10 == 0:
+                self.population = [Particle(-1.0, 1.0, self.dim) for _ in range(self.popsize)]
+                self.push(center)
+            else:
+                # pull
+                self.pull(center)
+
+            x = np.zeros(len(self.population))
+            y = np.zeros(len(self.population))
+
+            # evaluate
+            for i in range(len(self.population)):
+                # self.population[i].best = ackley(self.dim, self.population[i].coordination)
+                self.population[i].best = globals()[self.testing.lower()](self.dim, self.population[i].coordination)
+                if self.population[i].best <= center.best:
+                    center = self.population[i]
+                    self.max_mov = self.max_mov * self.reward_factor
+                x[i] = self.population[i].coordination[0]
+                y[i] = self.population[i].coordination[1]
+                if current_evl < self.evaluation:
+                    if center.best < best_so_far:
+                        best_so_far = center.best
+                    current_best[current_evl] = best_so_far
+                current_evl += 1
+            if center.best < best_so_far:
+                best_so_far = center.best
+
+            print(best_so_far)
+            # print('size :', self.popsize)
+            self.max_mov = self.max_mov * self.discount_factor
+            if self.max_mov > self.upper_bound - self.lower_bound:
+                self.max_mov = (self.upper_bound - self.lower_bound) * 0.7
+            k = k + 1
+
+            # fig, ax = plt.subplots()
+            # plt.axis([self.lower_bound, self.upper_bound, self.lower_bound, self.upper_bound])
+            # plt.plot(x, y, 'o')
+            # plt.show()
+
+        print(self.testing , best_so_far)
+        return current_best
+
+    def run_algo(self):
+        recorder = np.zeros((self.run, self.evaluation), dtype=float)
+        # print(recorder)
+        # print(recorder.shape)
+        for i in range(self.run):
+            algo = MyAlgo(dim=self.dim, testing=self.testing)
+            recorder[i] = algo.algo()
+            print(self.testing, 'run :', i)
+
+        # print(recorder)
+        # x = np.sum(recorder, axis=0)
+        x = np.mean(recorder, axis=0)
+        # print(x)
+
+        # x1 = np.arange(self.iteration)
+        # plt.plot(x1, x)
+        # plt.show()
+        pd.DataFrame(x).to_csv("./output/" + self.testing, header=None)
 
 if __name__=='__main__':
     t = time.time()
-    test = True
+    test = False
     if not test:
         functions = ['Ackley', 'Rastrigin', 'Sphere', 'Rosenbrock', 'Michalewitz', 'Griewank', 'Schwefel', 'Sum_squares', 'Zakharov', 'Powell']
         threads = []
--- plot.py
+++ plot.py
@@ -3,17 +3,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 if __name__=='__main__':
-    functions = ['Ackley', 'Rastrigin', 'Sphere', 'Rosenbrock', 'Michalewitz']
+    functions = ['Ackley', 'Rastrigin', 'Sphere', 'Rosenbrock', 'Michalewitz', 'Griewank', 'Schwefel', 'Sum_squares', 'Zakharov', 'Powell']
+    y_lim = {'Ackley' : [0, 15], 'Rastrigin' : [0,200], 'Sphere': [0,10], 'Rosenbrock':[0,400], 'Michalewitz':[-30, -5], 'Griewank':[0,10],
+                'Schwefel':[3000,10000], 'Sum_squares':[0,10], 'Zakharov':[0,80], 'Powell':[0, 150]}
     for f in functions:
-        x1 = np.zeros(4000, dtype=float)
-        x2 = np.zeros(4000, dtype=float)
-        x3 = np.zeros(4000, dtype=float)
+        x1 = np.zeros(400000, dtype=float)
+        x2 = np.zeros(400000, dtype=float)
+        x3 = np.zeros(400000, dtype=float)
         t = pd.read_csv('./output/' + f, index_col=0, header=None)
         t = t.to_numpy()
 
         for i in range(len(t)):
             x1[i] = t[i][0]
-        t = pd.read_csv('./output1/' + f.lower(), index_col=0, header=None)
+        t = pd.read_csv('./output_BBC/' + f.lower(), index_col=0, header=None)
         t = t.to_numpy()
         for i in range(len(t)):
             x2[i] = t[i][0]
@@ -21,20 +23,25 @@ if __name__=='__main__':
         t = t.to_numpy()
         for i in range(len(t)):
             x3[i] = t[i][0]
-        x = [i for i in range(4000)]
+        x1 = x1[::100]
+        x2 = x2[::100]
+        x3 = x3[::100]
+        print(x2)
+        print(x3)
+        x = [i*100 for i in range(len(x1))]
         plt.title(f, fontsize=38)
-        plt.xticks(fontsize=38)
-        plt.yticks(fontsize=38)
-        plt.xlabel('Iteration', fontsize=38)
+        plt.xticks(fontsize=30)
+        plt.yticks(fontsize=30)
+        plt.ylim(y_lim[f][0], y_lim[f][1])
+        plt.xlabel('Evaluations', fontsize=38)
         plt.ylabel('Fitness', fontsize=38)
         plt.grid(True)
         plt.plot(x, x1, label='AB3C', linewidth=6.0)
-        plt.plot(x, x2, label='SEMSO', linewidth=6.0)
-        plt.plot(x, x3, label='PSO', linewidth=6.0)
+        plt.plot(x, x2, label='N_AB3C', linewidth=5.0)
+        plt.plot(x, x3, label='PSO', linewidth=5.0)
         plt.legend(fontsize=38)
         
         plt.show()
-
     # x = np.zeros(4000, dtype=float)
     # t = pd.read_csv('./output1/Rastrigin', index_col=0, header=None)
     # t = t.to_numpy()
